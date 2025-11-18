45b96497408f3cf6f9cd12f2014292f001c12580
SUDARSHAN_REDDY <jsudarshanreddy2003@gmail.com>
2025-11-18 19:22:13 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,27 @@ import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['@fortawesome/react-fontawesome', 'framer-motion', 'gsap'],
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
